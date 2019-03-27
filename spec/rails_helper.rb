b8cfdc2e500106f2e9853fa9c411397775c1b59e
Rudy Ravelin <rudy@noukod.com>
2019-03-27 16:14:21 +0000

    Automatically backed up by Learn

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@
 # require 'webmock/rspec'
 # require 'support/github_response.rb'
 # # Add additional requires below this line. Rails is not loaded until this point!
-# 
+#
 # # Requires supporting ruby files with custom matchers and macros, etc, in
 # # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # # run as spec files by default. This means that files in spec/support that end
@@ -23,22 +23,22 @@
 # # require only the support files necessary.
 # #
 # # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
-# 
+#
 # # Checks for pending migration and applies them before tests are run.
 # # If you are not using ActiveRecord, you can remove this line.
 # ActiveRecord::Migration.maintain_test_schema!
 # WebMock.disable_net_connect!(:allow_localhost => true)
-# 
+#
 # RSpec.configure do |config|
 #   config.include GithubHelpers
 #   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
 #   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-# 
+#
 #   # If you're not using ActiveRecord, or you'd prefer not to run each of your
 #   # examples within a transaction, remove the following line or assign false
 #   # instead of true.
 #   config.use_transactional_fixtures = true
-# 
+#
 #   # RSpec Rails can automatically mix in different behaviours to your tests
 #   # based on their file location, for example enabling you to call `get` and
 #   # `post` in specs under `spec/controllers`.
@@ -53,7 +53,7 @@
 #   # The different available types are documented in the features, such as in
 #   # https://relishapp.com/rspec/rspec-rails/docs
 #   config.infer_spec_type_from_file_location!
-# 
+#
 #   # Filter lines from Rails gems in backtraces.
 #   config.filter_rails_from_backtrace!
 #   # arbitrary gems may also be filtered via:
